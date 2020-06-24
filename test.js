134e2483b5a74b5d23bba7857b1f6f7641d9e8df
vadkasevas <vadkasevas@mail.ru>
2020-06-24 23:32:27 +0300

    edit

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 const {nerdvision} = require('@nerdvision/agent')
-var i = 0;
 nerdvision.init('804026a8c624b17f9f32773856de36cf81307e80f86e24fd7e4423ccd5b25e6f523ff6838c78af2e7cd3ba9fb6ec7f1981256a5ca37a7c994eb6c84e829c1b2b')
 setInterval(()=>{
-    console.log(new Date(),i);
+    var i =0;
+    var j=1;
+    var d = String(new Date());
+    console.log(d,i,j);
 },1000);
